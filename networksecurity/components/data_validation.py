8a3e905da4c97c591a978c493f2d94cae67cd4c7
Ranveer Singh Rajput <singhrajputranveer068@gmail.com>
2026-02-27 22:23:25 +0530

    Data transformation commit

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -102,12 +102,12 @@ class DataValidation:
             )
             data_validation_artifact = DataValidationArtifact(
                 validation_status=status,
-                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
-                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                valid_train_file_path=self.data_validation_config.valid_train_file_path,
+                valid_test_file_path=self.data_validation_config.valid_test_file_path,
                 invalid_train_file_path=None,
                 invalid_test_file_path=None,
                 drift_report_file_path=self.data_validation_config.drift_report_file_path,
-                )
+            )
                       
             return data_validation_artifact
         except Exception as e:
