5532218bd9f867b463b8450386439d6a749230a9
Ranveer Singh Rajput <singhrajputranveer068@gmail.com>
2026-03-19 23:57:08 +0530

    pipeline is completed

--- networksecurity/components/Model_trainer.py
+++ networksecurity/components/Model_trainer.py
@@ -26,6 +26,9 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='RanveerSinghRajput024', repo_name='networksecurity', mlflow=True)
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
@@ -37,7 +40,7 @@ class ModelTrainer:
     def track_mlflow(self, Best_model, classificationmetric):
 
         print("MLFLOW RUN STARTED") 
-        
+
         mlflow.set_experiment("NetworkSecurity")
 
         with mlflow.start_run():
@@ -116,6 +119,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
+
+        save_object("final_model/model.pkl",best_model)
 
         ## Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
